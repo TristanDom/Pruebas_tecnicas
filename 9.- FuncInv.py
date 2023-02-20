3114bd9d20eced9cbecc95605a15e741a98d4ee5
TristanDom <tortad1700@alumno.ipn.mx>
2023-02-20 14:07:33 -0600

    UPDATE AND UPLOAD

--- 9.- FuncInv.py
+++ 9.- FuncInv.py
@@ -6,12 +6,7 @@ debería devolver la cadena "odnaborp yotse"""
 string = "estoy probando" #Se crea la variable de cadena de texto.
 
 def inversa(str): #Se define la función inversa para revertir las cadenas de texto.
-    cont = -1 #Se crea la variable cont que funciona como contador para comenzar desde el último elemento de la cadena de texto.
-    reverseString = "" #Variable para almacenar la cadena de texto revertida.
-    for i in str: #Bucle para ejecutar la cuenta regresiva de la cadena.
-        reverseString += str[cont] #Almacena el caracter desde el final al principio.
-        cont -= 1 #Resta uno para que la cuenta aumente y se pueda usar valores a la derecha.
+    cadena_inversa = str[::-1] #Almacena en la variable cadena inversa el inverso de la cadena.
+    return cadena_inversa #Retorna la palabra inversa.
 
-    print(reverseString) #Imprime la cadena de texto inversa.
-
-inversa(string) #Se llama a la función creada pasando el parámetro de cadena de texto.
+print(inversa(string)) #Se llama a la función creada pasando el parámetro de cadena de texto.
